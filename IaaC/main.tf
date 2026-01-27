2c71e30d582ef9907b3427ce7edbbfb4c8458112
lakshmi <lakshmichennur22@gmail.com>
2026-01-27 16:50:32 +0530

    commit7

--- IaaC/main.tf
+++ IaaC/main.tf
@@ -170,7 +170,10 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
+resource "aws_iam_role_policy_attachment" "lambda_vpc" {
+  role      = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
 ########################################
 # LAMBDA FUNCTION
 ########################################
