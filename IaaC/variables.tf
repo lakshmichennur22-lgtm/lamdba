7150da809cbce56533b612f65b1085d597c74604
lakshmi <lakshmichennur22@gmail.com>
2026-01-27 16:25:08 +0530

    commit3

--- IaaC/variables.tf
+++ IaaC/variables.tf
@@ -9,3 +9,4 @@ variable "environment"       { default = "dev" }
 variable "location"          { default = "india" }
 variable "location_short"    { default = "aps1" }
 variable "blockcode"         { default = "sms01" }
+variable "db_password"       
