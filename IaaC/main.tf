738412e2898ad3ce7ce27512e5ba9a91bf3ac5b4
lakshmi <lakshmichennur22@gmail.com>
2026-01-29 16:12:54 +0530

    commit26

--- IaaC/main.tf
+++ IaaC/main.tf
@@ -128,7 +128,14 @@ resource "aws_security_group" "rds_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.lambda_sg.id]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
 
 ########################################
 # RDS MYSQL
